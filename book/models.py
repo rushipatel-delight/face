f99ce94dd4fdcb21e246da686b4c979ef2f43664
[rushipatel.delight] <rushipatel.delight@gmail.com>
2016-07-28 14:16:33 -0700

    adding super user

--- book/models.py
+++ book/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# class Register_usermodel(models.Model):
+# 	username = models.CharField(max_length=100)
+# 	email = models.CharField(max_length=1000)
+# 	password = models.CharField(max_length=200)
+
+# 	def __str__(self):
+# 		return '%s %s %s'%(self.username, self.email, self.password)
